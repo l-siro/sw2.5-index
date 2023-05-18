3df97df035147fb77668ebac5651789562b06b7f
l-siro <122498760+l-siro@users.noreply.github.com>
2023-05-18 11:10:18 +0900

    パスワード設定

---
+++ script.js
@@ -0,0 +1,87 @@
+
+document.body.style.display = 'none';
+window.onload = function() {
+  var UserInput = null;
+
+  var cookies = document.cookie;
+  var cookiesArray = cookies.split(';');
+  var mi = 'ZnV3YWZ1d2FPbXVyYWlzdQ==';
+
+  for(var c of cookiesArray){
+    var cArray = c.split('=');
+    if(cArray[0].indexOf('cruw-basic') > -1){
+      UserInput = decodeURIComponent(cArray[1]);
+    }
+  }
+  if(!(UserInput && UserInput == window.atob(mi))){
+    UserInput = prompt("パスワードを入力して下さい:","");
+  }
+
+  if(UserInput != window.atob(mi)){
+    document.body.innerHTML = "403 Forbidden";
+  }else{
+    var now = new Date();
+    now.setMinutes(now.getMinutes() + 60*24*3);
+    document.cookie = "cruw-basic=" + encodeURIComponent(UserInput) + ";expires=" + now.toUTCString()+"; path=/;";
+  }
+  document.body.style.display = null;
+}
+
+
+const input = document.getElementById('searchInput');
+const bookContainer = document.getElementById('bookContainer');
+
+// データをローカルストレージから取得
+fetch('books.json') // あなたのJSONデータのURLをここに書きます
+    .then(response => response.json())
+    .then(books => {
+  // データをDOMに描画
+  books.forEach(book => {
+      const div = document.createElement('div');
+      div.classList.add('word-item');
+
+      const h2 = document.createElement('h2');
+      h2.classList.add('word-item__ttl');
+      h2.innerText = book.heading;
+
+      const divTitle = document.createElement('div');
+      divTitle.classList.add('word-item__book');
+      divTitle.innerText = book.title;
+
+      const divPages = document.createElement('div');
+      divPages.classList.add('word-item__pages');
+      divPages.innerHTML = 'ページ: <span>' + book.pages.join('. ') + '</span>';
+
+      const divTags = document.createElement('div');
+      divTags.classList.add('word-item__tags');
+      divTags.innerHTML = 'タグ: <span>' + book.tags.join('. ') + '</span>';
+
+      div.appendChild(h2);
+      div.appendChild(divTitle);
+      div.appendChild(divPages);
+      div.appendChild(divTags);
+
+      bookContainer.appendChild(div);
+  });
+
+// 検索機能を設定
+input.addEventListener('keyup', function() {
+    // 入力されたクエリを半角スペースと全角スペースで分割
+    const queries = this.value.toLowerCase().split(/[\s　]+/);
+
+    Array.from(bookContainer.getElementsByClassName('word-item')).forEach(book => {
+        const heading = book.getElementsByClassName('word-item__ttl')[0].innerText.toLowerCase();
+        const title = book.getElementsByClassName('word-item__book')[0].innerText.toLowerCase();
+        const pages = book.getElementsByClassName('word-item__pages')[0].innerText.toLowerCase();
+        const tags = book.getElementsByClassName('word-item__tags')[0].innerText.toLowerCase();
+
+        const bookData = heading + ' ' + title + ' ' + pages + ' ' + tags;
+
+        if (queries.every(query => bookData.includes(query))) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    });
+});
+});
